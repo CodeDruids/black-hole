14fa236fc960b4eb4ca32984cee71852b4f5ce20
Leith Caldwell <leith@codedruids.com>
2020-08-08 15:29:13 +1200

    Update documentation, tweak a couple of behaviours and add full unit tests

--- src/BlackHole.php
+++ src/BlackHole.php
@@ -13,73 +13,182 @@ namespace CodeDruids;
  */
 class BlackHole
 {
+    /**
+     * Constructor
+     *
+     * @see https://www.php.net/manual/en/language.oop5.decon.php#language.oop5.decon.constructor
+     *
+     * @return void
+     */
     public function __construct()
     {
         // deliberately empty
     }
 
+    /**
+     * Destructor
+     *
+     * @see https://www.php.net/manual/en/language.oop5.decon.php#language.oop5.decon.destructor
+     *
+     * @return void
+     */
     public function __destruct()
     {
         // deliberately empty
     }
 
+    /**
+     * Get class property (magic method)
+     *
+     * @see https://www.php.net/manual/en/language.oop5.overloading.php#object.get
+     *
+     * @param string $name  Property to retrieve
+     * @return mixed  The BlackHole instance so it can be chained.
+     */
     public function __get($name)
     {
         return $this;
     }
 
+    /**
+     * Set class property (magic method)
+     *
+     * @see https://www.php.net/manual/en/language.oop5.overloading.php#object.set
+     *
+     * @param string $name   Property to set
+     * @param mixed  $value  Value to set property to
+     * @return void
+     */
     public function __set($name, $value)
     {
-        return $this;
+        // deliberately empty
     }
 
+    /**
+     * Unset class property (magic method)
+     *
+     * @see https://www.php.net/manual/en/language.oop5.overloading.php#object.isset
+     *
+     * @param string $name  Property to check
+     * @return bool  Always true
+     */
     public function __isset($name)
     {
         return true;
     }
 
+    /**
+     * Unset class property (magic method)
+     *
+     * @see https://www.php.net/manual/en/language.oop5.overloading.php#object.unset
+     *
+     * @param string $name  Property to unset
+     * @return void
+     */
     public function __unset($name)
     {
         // deliberately empty
     }
 
+    /**
+     * Call specific class method in object context (magic method)
+     *
+     * @see https://www.php.net/manual/en/language.oop5.overloading.php#object.call
+     *
+     * @param string  $name       Method to call
+     * @param mixed[] $arguments  Enumerated array of method parameters
+     * @return mixed  The BlackHole instance so it can be chained.
+     */
     public function __call($name, $arguments)
     {
         return $this;
     }
 
+    /**
+     * Call specific class method in static context (magic method)
+     *
+     * @see https://www.php.net/manual/en/language.oop5.overloading.php#object.callstatic
+     *
+     * @param string  $name       Method to call
+     * @param mixed[] $arguments  Enumerated array of method parameters
+     * @return mixed  A new BlackHole instance so it can be chained.
+     */
     public static function __callStatic($name, $arguments)
     {
         return new static;
     }
 
+    /**
+     * Serialize class to string (magic method)
+     *
+     * @see https://www.php.net/manual/en/language.oop5.magic.php#object.tostring
+     *
+     * @return string  The fully qualified class name for the object
+     */
     public function __toString()
     {
         return static::class;
     }
 
+    /**
+     * Serialize class to array (magic method)
+     *
+     * @see https://www.php.net/manual/en/language.oop5.magic.php#object.serialize
+     *
+     * @return mixed  Associative array of key-value pairs as object representation
+     */
     public function __serialize()
     {
         return [];
     }
 
+    /**
+     * Unserialize array of class properties (magic method)
+     *
+     * @see https://www.php.net/manual/en/function.unserialize.php
+     *
+     * @param mixed[] $data  Properties to unserialize back into the class
+     * @return void
+     */
     public function __unserialize(array $data)
     {
         // deliberately empty
     }
 
+    /**
+     * Call object as a function (magic method)
+     *
+     * @see https://www.php.net/manual/en/language.oop5.magic.php#object.invoke
+     *
+     * @return string  The BlackHole instance so it can be chained.
+     */
     public function __invoke()
     {
         return $this;
     }
 
+    /**
+     * Override object representation for {@see \var_export()} (magic method)
+     *
+     * @see https://www.php.net/manual/en/language.oop5.magic.php#object.set-state
+     *
+     * @param mixed[] $properties  Associative array of exported properties
+     * @return object  A new BlackHole instance so it can be chained.
+     */
     public static function __set_state($properties)
     {
         return new static;
     }
 
+    /**
+     * Override object representation for {@see \var_dump()} (magic method)
+     *
+     * @see https://www.php.net/manual/en/language.oop5.magic.php#object.debuginfo
+     *
+     * @return mixed[]  Only the class name
+     */
     public function __debugInfo()
     {
-        return [];
+        return ['class' => static::class];
     }
 }
--- tests/BlackHoleTest.php
+++ tests/BlackHoleTest.php
@@ -12,10 +12,114 @@ class BlackHoleTest extends TestCase
     public function testConstructor()
     {
         $empty = new BlackHole();
-        $test = new BlackHole("test");
+        $test = new BlackHole('test');
 
         $this->assertInstanceOf(BlackHole::class, $empty);
         $this->assertInstanceOf(BlackHole::class, $test);
         $this->assertEquals($empty, $test);
+    }
+
+    /**
+     * @test
+     */
+    public function testDestructor()
+    {
+        $test = new BlackHole();
+        $this->assertInstanceOf(BlackHole::class, $test);
+        unset($test);
+        $this->assertFalse(isset($test));
+    }
+
+    /**
+     * @test
+     */
+    public function testPropertyOverloading()
+    {
+        $empty = new BlackHole();
+        $test = new BlackHole();
+
+        // get
+        $this->assertSame($test, $test->some_property);
+        // set
+        $test->some_property = 'test';
+        $this->assertEquals($empty, $test);
+        // isset
+        $this->assertTrue(isset($test->some_other_property));
+        // unset
+        unset($test->some_property);
+        $this->assertEquals($empty, $test);
+    }
+
+    /**
+     * @test
+     */
+    public function testMethodOverload()
+    {
+        // object context
+        $test = new BlackHole();
+        $result = $test->undefinedMethod('test');
+        $this->assertSame($test, $result);
+        // static context
+        $empty = new BlackHole();
+        $result = BlackHole::undefinedStaticMethod('test');
+        $this->assertInstanceOf(BlackHole::class, $result);
+        $this->assertEquals($empty, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function testSerializationOverloading()
+    {
+        // to string
+        $test = new BlackHole();
+        $test->some_other_property = "test";
+        $this->assertEquals(BlackHole::class, (string) $test);
+        // serialize
+        $this->assertEquals('O:20:"'.BlackHole::class.'":0:{}', serialize($test));
+        // unserialize
+        $empty = new BlackHole();
+        $result = unserialize(
+            'O:20:"'.BlackHole::class.'":1:{s:4:"prop";s:3:"val";}',
+            ['allowed_classes' => [BlackHole::class]]
+        );
+        $this->assertInstanceOf(BlackHole::class, $result);
+        $this->assertEquals($empty, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function testObjectAsFunction()
+    {
+        $test = new BlackHole();
+        $this->assertSame($test, $test());
+    }
+
+    /**
+     * @test
+     */
+    public function testObjectExport()
+    {
+        $empty = new BlackHole();
+        $this->assertEquals(BlackHole::class."::__set_state(array(\n))", var_export($empty, true));
+        $result = BlackHole::__set_state(['property' => 'value']);
+        $this->assertInstanceOf(BlackHole::class, $result);
+        $this->assertEquals($empty, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function testObjectDump()
+    {
+        $test = new BlackHole();
+        $expected = BlackHole::class." Object\n(\n    [class] => ".BlackHole::class."\n)\n";
+        $this->assertEquals($expected, print_r($test, true));
+        ob_start();
+        var_dump($test);
+        $result = ob_get_clean();
+        $this->assertStringStartsWith(__FILE__.":120:\nclass ".BlackHole::class."#", $result);
+        $this->assertStringEndsWith(" (0) {\n}\n", $result);
     }
 }
